7d029cc54193150ab42ef6c743d9405581e4b300
Liam Williams <liam.williams@zoho.com>
2016-04-03 15:07:46 +0100

    Updated to integrate with new zohhak api changes

--- src/main/java/io/github/theangrydev/yatspeczohhakplugin/ZohhakParameterResolver.java
+++ src/main/java/io/github/theangrydev/yatspeczohhakplugin/ZohhakParameterResolver.java
@@ -1,32 +1,37 @@
+
 package io.github.theangrydev.yatspeczohhakplugin;
 
 import com.googlecode.yatspec.junit.ParameterResolver;
 import com.googlecode.yatspec.junit.Row;
 import com.googlecode.yatspec.junit.VarargsParameterResolver;
-import com.googlecode.zohhak.api.DefaultConfiguration;
+import com.googlecode.zohhak.api.DefaultCoercer;
+import com.googlecode.zohhak.api.backend.ConfigurationBuilder;
+import com.googlecode.zohhak.api.backend.ConfigurationResolver;
 import com.googlecode.zohhak.api.backend.ParameterCalculator;
 import com.googlecode.zohhak.api.backend.ParameterCalculatorProvider;
 import io.github.theangrydev.yatspeczohhakplugin.json.JsonCollectionsParameterCoercerFactory;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Method;
+import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static java.util.Arrays.stream;
+import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.joining;
 
 @SuppressWarnings("WeakerAccess")
 public class ZohhakParameterResolver implements ParameterResolver {
 
+    private static final String STRING_BOUNDARY = "'";
     private static final String PARAMETER_DELIMITER = "~";
     private static final String COLLECTION_DELIMITER = ",";
     private static final String COLLECTION_BEGIN = "[";
     private static final String COLLECTION_END = "]";
 
     private final VarargsParameterResolver varargsParameterResolver = new VarargsParameterResolver();
-    private final ParameterCalculator parameterCalculator = new ParameterCalculatorProvider().getExecutor(new JsonCollectionsParameterCoercerFactory());
-    private final CustomConfiguration customConfiguration = new CustomConfiguration();
+    private final ParameterCalculator parameterCalculator = new ParameterCalculatorProvider().getExecutor(new JsonCollectionsParameterCoercerFactory(), new CustomConfigurationResolver());
 
     @Override
     public Object[] resolveParameters(Row row, Class<?> testClass, Method testMethod) throws Exception {
@@ -51,13 +56,33 @@ public class ZohhakParameterResolver implements ParameterResolver {
     }
 
     private Object[] resolveParameters(Method testMethod, String parametersLine) throws NoSuchMethodException {
-        return parameterCalculator.calculateParameters(parametersLine, testMethod, customConfiguration);
+        return parameterCalculator.calculateParameters(parametersLine, testMethod);
     }
 
-    private static class CustomConfiguration extends DefaultConfiguration {
+    private static class CustomConfigurationResolver implements ConfigurationResolver {
+
         @Override
-        public String separator() {
+        public ConfigurationBuilder calculateConfiguration(Method testMethod) {
+            return new CustomConfiguration();
+        }
+    }
+
+    private static class CustomConfiguration implements ConfigurationBuilder {
+
+        @Override
+        public List<Class<?>> getCoercers() {
+            return singletonList(DefaultCoercer.class);
+        }
+
+        @Override
+        public String getParameterSeparator() {
             return PARAMETER_DELIMITER;
+        }
+
+        @Override
+        public String getStringBoundary() {
+            return STRING_BOUNDARY;
         }
     }
 }
+
